b0d0f47986e45e282cba8d2e845822fd0deac0e0
fadai-0110 <94634319+fadai-0110@users.noreply.github.com>
2023-05-21 18:25:16 +0800

    Update train.py

--- train.py
+++ train.py
@@ -442,7 +442,7 @@ def parse_opt(known=False):
     parser.add_argument('--weights', type=str, default=ROOT / './pretrained/yolov5s.pt', help='initial weights path')
     parser.add_argument('--cfg', type=str, default=ROOT / './models/yolov5s.yaml', help='model.yaml path')
     parser.add_argument('--data', type=str, default=ROOT / './data/data.yaml', help='dataset.yaml path')
-    parser.add_argument('--hyp', type=str, default=ROOT / './data/hyps/hyp.scratch.yaml', help='hyperparameters path')
+    parser.add_argument('--hyp', type=str, default=ROOT / './data/hyps/hyp.scratch-high.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--batch-size', type=int, default=32, help='total batch size for all GPUs, -1 for autobatch')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
