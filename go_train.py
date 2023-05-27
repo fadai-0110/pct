30ec0a2802f6586bac250e18229e34aa6f29fd69
fadai-0110 <94634319+fadai-0110@users.noreply.github.com>
2023-05-27 11:56:13 +0800

    Update go_train.py

--- go_train.py
+++ go_train.py
@@ -1,5 +1,5 @@
 import os
-os.system("python ./pct/train.py --data ./pct/data/my_data.yaml --cfg ./pct/models/yolov5s.yaml --weights ./pct/pretrained/yolov5s.pt --epoch 100 --batch-size 16 --device 0,1")
+os.system("python ./pct/train.py --data ./pct/data/my_data.yaml --cfg ./pct/models/yolov5s.yaml --weights ./pct/pretrained/yolov5s.pt --epoch 100 --batch-size 32 --device 0,1")
 # os.system("python train.py --data my_data.yaml --cfg yolov5m.yaml --weights pretrained/yolov5m.pt --epoch 100 --batch-size 4")
 # os.system("python train.py --data my_data.yaml --cfg yolov5l.yaml --weights pretrained/yolov5l.pt --epoch 100 --batch-size 4")
 
